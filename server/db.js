497b4d330d8d7ac39a7c94517a69d77ac89a5e13
ZoyaLatif <124717752+ZoyaLatif@users.noreply.github.com>
2023-04-02 16:06:14 +0100

    Add files via upload

---
+++ server/db.js
@@ -0,0 +1,14 @@
+const Pool = require("pg").Pool;
+require('dotenv').config()
+
+const pool = new Pool ({
+    user: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    host: process.env.HOST,
+    port:process.env.DBPORT,
+    database: "todoapp",
+
+
+})
+
+module.exports=pool;
